e05da395b1e31138cf5e6d5054d4d77a16968c68
Bimal <anshul.dbgt@gmail.com>
2016-12-28 16:56:05 +0530

    email sent

--- src/notification.py
+++ src/notification.py
@@ -1,5 +1,12 @@
 
 import os
+import smtplib
+
+
+gmail_user = ""  #enter the email id to send from
+gmail_pwd = ""   #enter the password of that account
+
+
 class Notification:
     """
     Interface which allows user to send notifications.
@@ -15,6 +22,7 @@ class Notification:
         """
         recipients = open("recipients.txt","a") #: file containing a list of recipients or their emails
         recipients.close()
+        
 
     def add_recipient(self,recipient):
         """
@@ -27,17 +35,30 @@ class Notification:
 
     def send_notification(self, message, recipient):
         """
-        An email address will be sent to the email address provided as recipeient.
+        An email address will be sent to the email address provided as recipient.
 
         """
         print(message, recipient)
+        TO = recipient
+        SUBJECT = "Notification from Vulnerability"
+        TEXT = message
+        server = smtplib.SMTP('smtp.gmail.com', 587)
 
-        pass
+        server.starttls()
+        server.ehlo()
+        server.login(gmail_user, gmail_pwd)
+        BODY = '\r\n'.join(['To: %s' % TO,
+                'From: %s' % gmail_user,
+                'Subject: %s' % SUBJECT,
+                '', TEXT])
+
+        server.sendmail(gmail_user, [TO], BODY)
+        print ('email sent')
 
 
 if __name__ == "__main__":
     notification_sender = Notification()
     message = "Message I want to send"
     source = "Who I want to send the message to. Most likely an email address??"
-    notification_sender.add_recipient("anshul7@vt.edu")
+    notification_sender.send_notification("Hi",'anshul.dbgt@gmail.com')
     #notification_sender.send_notification("This is the message I want to send")
