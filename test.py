823279d36a74d206dfbf5b10e7343dc5f03e953b
Mike Feneley <michael.j.feneley@gmail.com>
2017-01-08 07:45:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 
 print(sys.path)
-sys.path.append('/usr/lib/python2.7/dist-packages')
-print(sys.path)
+#sys.path.append('/usr/lib/python2.7/dist-packages')
+#print(sys.path)
 import mysql
+print("IMPORT?")
