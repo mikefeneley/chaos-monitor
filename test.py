16e9b1e8fe282a8ffbe2baba4b4a129a7e8971d4
Mike Feneley <michael.j.feneley@gmail.com>
2017-01-08 08:12:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sys
 print(sys.path)
 #sys.path.append('/usr/lib/python2.7/dist-packages')
 try:
-    import mysql
+    import mysql.connector
     print("Imported")
 except Exception as e:
     print(e)
