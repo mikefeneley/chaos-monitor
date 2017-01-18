e1be357e12531983d8019442a15b3a7724c5c3b8
A Student <michael.j.feneley@gmail.com>
2017-01-18 17:24:15 -0500

    Remove test data files

--- src/server/ftp_handler.py
+++ src/server/ftp_handler.py
@@ -10,7 +10,17 @@ class MyHandler(FTPHandler):
 
     def on_connect(self):
         print "%s:%s connected" % (self.remote_ip, self.remote_port)
-        
+
+    def on_disconnect(self):
+        monitored_files = open('files', 'r')
+        for line in monitored_files:
+            abspath = line.strip("\n")
+            tmp = abspath.split("/")
+            afile = tmp[len(tmp) - 1]
+            os.remove(afile)
+        monitored_files.close()
+
+    
     def on_login(self, username):
         monitored_files = open('files', 'r')
         
@@ -22,7 +32,6 @@ class MyHandler(FTPHandler):
         monitored_files.close()
 
     def on_logout(self, username):
-        # do something when client disconnects
         monitored_files = open('files', 'r')
 
         for line in monitored_files:
